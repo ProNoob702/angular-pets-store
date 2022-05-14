a5d07af962d089740ff1937261920f269164f478
taha.seddik1992@gmail.com <taha.seddik1992@gmail.com>
2022-05-14 17:14:53 +0100

    routing is ok

--- src/app/pet-detail/pet-detail.component.ts
+++ src/app/pet-detail/pet-detail.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IPet, petsList } from 'src/pets';
 
 @Component({
   selector: 'app-pet-detail',
   templateUrl: './pet-detail.component.html',
-  styleUrls: ['./pet-detail.component.scss']
+  styleUrls: ['./pet-detail.component.scss'],
 })
 export class PetDetailComponent implements OnInit {
+  pet: IPet | undefined;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    // First get the product id from the current route.
+    const routeParams = this.route.snapshot.paramMap;
+    const routePetId = Number(routeParams.get('id'));
+    // Find the product that correspond with the id provided in route.
+    this.pet = petsList.find((p) => p.id === routePetId);
   }
-
 }
